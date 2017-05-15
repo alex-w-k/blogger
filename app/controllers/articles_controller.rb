588191b5133df28f07c461cd7879f5fbaed97587
Alex Koch <alex549us3@gmail.com>
2017-05-14 21:30:53 -0600

    add new article done 🎉🎉🎉

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,27 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+    @article = Article.find(params[:id]) 
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to action: :index
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
